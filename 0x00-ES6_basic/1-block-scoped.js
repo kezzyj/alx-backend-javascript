43826a6552639d8c743c8191f8828919cca0ba64
kezzyj <karachidavid31@gmail.com>
2023-01-25 16:24:00 +0000

    perfecting var with  hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,21 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-
   var task = false;
 
   var task2 = true;
 
-
-
   if (trueOrFalse) {
+    const task = true;
 
-    let task = true;
-
-    let task2 = false;
-
+    const task2 = false;
   }
 
-
-
   return [task, task2];
-
 }
