9e1eba371f595e7a1dfcacb0a9e834714ed357a1
kezzyj <karachidavid31@gmail.com>
2023-01-26 10:03:10 +0000

    using es6 object notation

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,12 @@
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const currentYear = new Date().getFullYear();
+
+  return {
+
+   [`income-${currentYear}`]: income,
+
+    [`gdp-${currentYear}`]: gdp,
+
+    [`capita-${currentYear}`]: capita,
+  };
+}
