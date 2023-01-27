f94b3246cec999f317c688bb09d3714edf715c08
kezzyj <karachidavid31@gmail.com>
2023-01-27 11:37:17 +0000

    resolving object if true

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,14 @@
-export default getFullResponseFromAPI(success){
-   return new Promise((resolve, reject) => {
-   setTimeout(() => {  
-   if (success) {
-    resolve({status: 200,body: 'Success'});
-   } else { 
-    reject("The fake API is not working currently");
-  }
- }, 0);
- });
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (success) {
+        resolve({
+	    status: 200,
+	    body: 'Success',
+        });
+      } else {
+        reject('The fake API is not working currently');
+      }
+    }, 0);
+  });
 }
