2db4559481d073ff355f5585558efa43869533c7
kezzyj <karachidavid31@gmail.com>
2023-01-30 19:38:47 +0000

    using reduce

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,15 @@
+const getStudentIdsSum = (students) => {
+
+  const id = students.reduce(
+
+    (acc, value) => acc + value.id, 0,
+
+  );
+
+  return id;
+
+};
+
+
+
+export default getStudentIdsSum;
