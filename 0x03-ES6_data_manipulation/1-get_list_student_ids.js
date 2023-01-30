6548c6dd1574b7f14fa846e1650077c472757610
kezzyj <karachidavid31@gmail.com>
2023-01-30 18:56:06 +0000

    return array of ids

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,15 @@
+const getListStudentIds = (myArray) => {
+
+  if (!Array.isArray(myArray)) {
+
+    return [];
+
+  }
+
+  return myArray.map((studentId) => studentId.id);
+
+};
+
+
+
+export default getListStudentIds;
