c80ad6d9c2f08bc33031d832a18adde35fd83cc4
kezzyj <karachidavid31@gmail.com>
2023-01-31 13:13:40 +0000

    using filter, spread, map, slices, substring

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 const cleanSet = (set, startString) => {
   if (startString === undefined || startString.length === 0 || startString === '') return '';
-  const filteredSet = [...set].filter((value) => value.startsWith(startString));
+  const filteredSet = [...set].filter((value) => (value !== undefined ? value.startsWith(startString) : ''));
   if (!filteredSet.length) return '';
-  return filteredSet.map((value) => value.slice(startString.length)).join('-');
+  return filteredSet.map((value) => (value !== undefined ? value.slice(startString.length) : '')).join('-');
 };
 export default cleanSet;
