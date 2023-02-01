b3a713b74ec9363fb2c43c3ce329d1223833e92c
kezzyj <karachidavid31@gmail.com>
2023-02-01 17:17:13 +0000

    alot of wrk done

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,11 @@
+import {RowElement,RowID } from './interface';
+
+
+
+
+
+declare function insertRow(row: RowElement): number;
+
+declare function deleteRow(rowId: RowID): void;
+
+declare function updateRow(rowId: RowID, row: RowElement): number;
